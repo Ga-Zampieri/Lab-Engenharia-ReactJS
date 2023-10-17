d69c41d107915ea9dcf5dc0015d82707ee643030
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2023-10-17 20:42:32 -0300

    Initial Commit

---
+++ my-app/src/Atividade02/Atividade02.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+function Atividade02() {
+  return (
+    <>
+      <h1>Atividade02</h1>
+      <span> <Link to={"/"}>Voltar</Link></span>
+    </>
+  );
+}
+
+export default Atividade02;
